7108dd04bee7198dec6ab9b28197ff78e17a8502
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-06-05 23:40:06 +0900

    ローカルLLMサーバーに対してリクエストする

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+from openai import OpenAI
+
+# ローカルサーバーに接続
+# api_keyはダミー値を設定
+client = OpenAI(
+    base_url="http://127.0.0.1:1234/v1",
+    api_key="lm-studio",
+)
+
 def main():
-    print("Hello from hello-lang-chain!")
+    completion = client.chat.completions.create(
+        model="google/gemma-3-27b",
+        messages=[
+            {"role": "system",
+             "content": "あなたは優秀なアシスタントです。あなたは人類最高知能を持つAIなので、人間の質問に対しても完璧に回答することが出来ます。"},
+            {"role": "user", "content": "自己紹介をしてください。"}
+        ],
+        temperature=0.7
+    )
+
+    # 実際の出力の表示
+    print(completion.choices[0].message.content)
 
 
 if __name__ == "__main__":
