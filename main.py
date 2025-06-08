e9c148b85e238149d009c4fd495b15c51016e230
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-06-08 19:41:00 +0900

    【add】数学の問題を用いて思考過程と結論を分離する例を追加

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain_core.messages import  SystemMessage
+
 from langchain_ollama import ChatOllama
+
+from pydantic import BaseModel, Field
+
+
+class OutputModel(BaseModel):
+    reasoning: str = Field(..., description='問題を解く上で必要な全ての思考内容と、最終的な結果を出力する')
+    conclusion: str = Field(..., description='これまでの思考結果から最終的な結論のみを出力する')
 
 
 def make_prompt(user_input: str) -> list:
@@ -19,9 +29,53 @@ model = ChatOllama(
 # プロンプトテンプレート ＋ モデル ＋ 出力パーサー
 chain = make_prompt | model | StrOutputParser()
 
+def answer_math_question():
+    system_prompt = """
+    回答する際は下記のように構造化して出力してください
+    - reasoning: ユーザの質問をステップバイステップで検討し、最終的な回答を推論し、全ての思考をこのフィールドに出力してください。
+    - conclusion: 最終的な回答のみを出力してください。
+    """
+
+    question = """
+        公園と学校の距離は1200mです。
+        A君が公園から、B君は学校から向かい合って同時に出発すると8分で出会いました。
+        また別の日、B君が学校から出た5分後にA君が追いかけるとA君が出発して10分後に追いつきました。
+        A君の速さは、「分速何m」でしょうか？
+        """
+    message = [
+        SystemMessage(content=system_prompt),
+        HumanMessagePromptTemplate.from_template(
+            [
+                {
+                    "type": "text",
+                    "text": "{question}"
+                },
+            ]
+        )
+    ]
+    prompt = ChatPromptTemplate.from_messages(message)
+
+    # memo モデルgemma3:27b-it-qatだと性能が足りず、正しい回答が得られない
+    model = ChatOllama(
+        model="gemma3:27b-it-qat",
+        temperature=0,
+        top_p=0.2,
+    )
+
+    chain = prompt | model.with_structured_output(OutputModel)
+    result = chain.invoke({"question": question})
+    print("====modelの出力====")
+    print("===思考過程===")
+    print(result.reasoning)
+    print("===結論===")
+    print(result.conclusion)
+
+
 def main():
     for chunk in chain.stream("日本で一番高い山は何ですか？"):
         print(chunk, end="", flush=True)
+
+    answer_math_question()
 
 if __name__ == "__main__":
     main()
