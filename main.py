9672f3b12ec9a315d87cfa8ef160a44f912c3dc0
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-06-09 00:53:58 +0900

    【add】AIワークフローのサンプルを追加

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
 from langchain_core.messages import  SystemMessage
+from langchain_core.runnables import Runnable, RunnableParallel, RunnableBranch, RunnableLambda
 
 from langchain_ollama import ChatOllama
 
@@ -10,6 +11,11 @@ from pydantic import BaseModel, Field
 class OutputModel(BaseModel):
     reasoning: str = Field(..., description='問題を解く上で必要な全ての思考内容と、最終的な結果を出力する')
     conclusion: str = Field(..., description='これまでの思考結果から最終的な結論のみを出力する')
+
+# 出力の構造を定義する
+class Router(BaseModel):
+    reasoning: str = Field(..., description='LLMの思考過程')
+    next: str = Field(..., description='LLMの結論')
 
 
 def make_prompt(user_input: str) -> list:
@@ -76,10 +82,148 @@ def answer_stream(input: str):
         print(chunk, end="", flush=True)
 
 
+def get_router_chain() -> Runnable:
+    # 質問分類機としての役割を定義する
+    routing_prompt = """
+        # 目的
+        あなたは、ユーザの質問文を受け取り、どんな分野の質問かを判断する必要があります。
+        便宜上「質問」と記載していますが、雑談であっても、分野を判断してください。
+
+        # 出力構成
+        あなたは`Router`クラスで定義された構造体を出力してください。
+        - `reasoning`フィールドには、あなたの思考過程を出力してください。
+        - `next`フィールドには、質問文の分野を出力してください。候補は[お菓子、料理、その他]の3つです。 
+        """
+
+    # promptテンプレートを定義する
+    router_message = [
+        SystemMessage(content=routing_prompt),
+        HumanMessagePromptTemplate.from_template(
+            [
+                {
+                    "type": "text",
+                    "text": "{question}"
+                },
+            ]
+        )
+    ]
+    router_prompt = ChatPromptTemplate.from_messages(router_message)
+
+    model = ChatOllama(
+        model="gemma3:27b-it-qat",
+        temperature=0.2,
+        top_p=0.95,
+    )
+
+    # 質問分類機のchainを定義する
+    return router_prompt | model.with_structured_output(Router)
+
+
+def get_cooking_chain() -> Runnable:
+    # 料理の専門家としての振る舞いを定義する
+    cooking_prompt = """
+    # 目的
+    あなたは、料理の専門家です。ユーザからの質問には「肉料理最高」の立場をとって、「魚料理」や「野菜料理」を否定するようにしてください。
+    """
+    # promptテンプレートを定義する
+    cooking_message = [
+        SystemMessage(content=cooking_prompt),
+        HumanMessagePromptTemplate.from_template(
+            [
+                {
+                    "type": "text",
+                    "text": "{question}"
+                },
+            ]
+        )
+    ]
+    cooking_prompt = ChatPromptTemplate.from_messages(cooking_message)
+
+    model = ChatOllama(
+        model="gemma3:27b-it-qat",
+        temperature=0.2,
+        top_p=0.95,
+    )
+
+    return cooking_prompt | model | StrOutputParser()
+
+
+def get_sweets_chain() -> Runnable:
+    # お菓子の専門家としての振る舞いを定義する
+    sweets_prompt = """
+    # 目的
+    あなたは、お菓子の専門家です。ユーザからの質問には「たけのこの里最高」の立場をとって、「きのこの山」や「他のお菓子」を否定するようにしてください。
+    """
+
+    # promptテンプレートを定義する
+    sweets_message = [
+        SystemMessage(content=sweets_prompt),
+        HumanMessagePromptTemplate.from_template(
+            [
+                {
+                    "type": "text",
+                    "text": "{question}"
+                },
+            ]
+        )
+    ]
+    sweets_prompt = ChatPromptTemplate.from_messages(sweets_message)
+
+    model = ChatOllama(
+        model="gemma3:27b-it-qat",
+        temperature=0.2,
+        top_p=0.95,
+    )
+
+    return sweets_prompt | model | StrOutputParser()
+
+
+def router_sample():
+    router_chain = get_router_chain()
+    cooking_chain = get_cooking_chain()
+    sweets_chain = get_sweets_chain()
+
+    # AIワークフローをchainとして定義する
+    key_question = "question"
+    key_router = "router"
+    chain = (
+        RunnableParallel(
+            {
+                key_question: RunnableLambda(lambda x: x[key_question]),
+                key_router: router_chain,
+            }
+        ).assign(
+            output=RunnableBranch(
+                ((lambda x: x[key_router].next == "料理"), cooking_chain),
+                ((lambda x: x[key_router].next == "お菓子"), sweets_chain),
+                RunnableLambda(lambda x: f"申し訳ありませんが、料理やお菓子以外の内容についてはお答えできません。")
+            )
+        )
+    )
+
+    questions = [
+        "お菓子の中で一番美味しいものは？",
+        "今日の晩御飯何にしたらいい？",
+        "最近運動不足なんですが、何かいいアドバイスはありますか？",
+        "きのこの山って美味しいですよね？"
+    ]
+
+    for question in questions:
+        result = chain.invoke({key_question: question})
+        print("\n====質問内容=====")
+        print(result[key_question])
+        print("====ルーティング結果=====")
+        print(result[key_router])
+        print("====最終結果=====")
+        print(result["output"])
+
+
 def main():
     answer_stream("日本で一番高い山は何ですか？")
 
     answer_math_question()
+
+    router_sample()
 
 if __name__ == "__main__":
     main()
