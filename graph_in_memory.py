283bd99ff53b769bebb85a4afbcf86bed9847568
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-06-15 13:09:00 +0900

    【modify】Tool Callingを利用し、TavilyによるWeb検索エージェントを作成
    ・ローカルLLMだと性能不足だったのでGeminiを用いるように修正
    ・python-dotenvは結局使用しなかったが、今後使うことがありそうなので依存関係には追加したままにしておく

--- graph_in_memory.py
+++ graph_in_memory.py
@@ -4,10 +4,13 @@ from typing import TypedDict, Annotated
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, SystemMessage, AnyMessage
 
-from langchain_ollama import ChatOllama
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from langchain_tavily import TavilySearch, TavilyExtract
 
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, END, StateGraph
+from langgraph.prebuilt import ToolNode
 
 class GraphState(TypedDict):
     # messagesがキー、list[AnyMessage]がバリュー
@@ -16,17 +19,30 @@ class GraphState(TypedDict):
     messages: Annotated[list[AnyMessage], operator.add]
 
 
-def create_langgraph(chain):
+def create_langgraph(chain, tools):
     def call_llm(state: GraphState):
         response = chain.invoke({"messages": state["messages"]})
+        print("====response====")
+        print(response)
         return {"messages": [response]}
+
+    def should_continue(state: GraphState):
+        messages = state["messages"]
+        last_message = messages[-1]
+        if last_message.tool_calls:
+            return "tools"
+        return END
 
     workflow = StateGraph(state_schema=GraphState)
     # nodeの追加
-    workflow.add_node("model", call_llm)
+    node_name_agent = "agent"
+    node_name_tools = "tools"
+    workflow.add_node(node_name_agent, call_llm)
+    workflow.add_node(node_name_tools, ToolNode(tools))
     # edgeの追加
-    workflow.add_edge(START, "model")
-    workflow.add_edge("model", END)
+    workflow.add_edge(START, node_name_agent)
+    workflow.add_conditional_edges(node_name_agent, path=should_continue, path_map=[node_name_tools, END])
+    workflow.add_edge(node_name_tools, node_name_agent)
     # グラフの状態をインメモリーに保存
     memory = MemorySaver()
     graph = workflow.compile(checkpointer=memory)
@@ -55,20 +71,43 @@ def conversation(graph):
         # 同じスレッドIDでinvokeが繰り返されることで、会話履歴が引き継がれる
         response = graph.invoke({"messages": input_query}, config={"configurable": {"thread_id": "12345"}})
 
+        # デバック用
+        print("response: ", response)
+
         print("=================================")
         print("AIの回答", response["messages"][-1].content)
 
 def main():
-    model = ChatOllama(
-        model="gemma3:27b-it-qat",
-        temperature=0.2,
-        top_p=0.95,
+    # orieg/gemma3-tools:27b-it-qatだと性能不足でtoolを活用することが出来なかった
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash-preview-04-17",
+        temperature=0,
+        max_retries=0,
     )
+
+    tavily_search_tool = TavilySearch(
+        max_results=10,
+        topic="general",
+    )
+
+    tavily_extract_tool = TavilyExtract()
+
+    tools = [
+        tavily_search_tool,
+        tavily_extract_tool,
+    ]
+
+    system_prompt = """
+    あなたは日本語を話す優秀なアシスタントです。回答には必ず日本語で答えてください。また考える過程も出力してください。
+    私たちは「tavily_search_tool」と「tavily_extract_tool」という2つのツールを持っています。
+    tavily_search_toolは、Google検索を行い、上位5件のURLや概要を取得するツールです。どんなwebサイトがあるかを浅く拾う場合にはこちらを利用します
+    tavily_extract_toolは、URLを指定して、ページの内容を抽出するツールです。特定のWebサイトのURLがわかっており、詳細に内容を取得する場合はこちらを利用します。
+    適切に利用してユーザからの質問に回答してください。
+    """
 
     # messageを作成する
     message = [
-        SystemMessage(
-            content="あなたは日本語を話す優秀なアシスタントです。回答には必ず日本語で答えてください。また考える過程も出力してください。"),
+        SystemMessage(content=system_prompt),
         # メッセージのリスト（会話履歴）を動的に挿入するためのplaceholder
         MessagesPlaceholder("messages"),
     ]
@@ -77,8 +116,8 @@ def main():
     prompt = ChatPromptTemplate.from_messages(message)
 
     # chainとgraphを作成
-    chain = prompt | model
-    graph = create_langgraph(chain)
+    chain = prompt | llm.bind_tools(tools)
+    graph = create_langgraph(chain, tools)
 
     conversation(graph)
 
